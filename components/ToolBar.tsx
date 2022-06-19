e8e285ac549906404349241b3b78fead742ae928
Johan Pedro Inês <johanpedroo@gmail.com>
2022-06-19 20:42:24 -0300

    add icons on page name

--- components/ToolBar.tsx
+++ components/ToolBar.tsx
@@ -1,7 +1,9 @@
 import Link from "next/link";
 import {FC, ReactElement} from "react";
 import {Box, Flex, Text, IconButton , useBreakpointValue, useColorMode, useColorModeValue} from "@chakra-ui/react";
-import {BsMoonFill, BsSunFill} from "react-icons/bs";
+import {BsGear, BsMoonFill, BsSunFill} from "react-icons/bs";
+import {Icon} from "@chakra-ui/icons";
+import {TbHammer} from "react-icons/tb";
 
 const ToolBar: FC = (): ReactElement => {
     const { colorMode, toggleColorMode } = useColorMode()
@@ -11,7 +13,7 @@ const ToolBar: FC = (): ReactElement => {
             <Flex flexDirection={"row"} justifyContent={'space-between'} flex={1} alignItems={"center"}>
                 <Link href={"/"}>
                     <a>
-                        <Text fontSize={useBreakpointValue({md: '4xl', xs: '2xl'})} fontWeight={"bold"}>Awesome Tools</Text>
+                        <Text fontSize={useBreakpointValue({md: '4xl', xs: '2xl'})} fontWeight={"bold"}>Awes<Icon as={BsGear} fontSize="0.7em" style={{display: 'inline'}}/>me <Icon as={TbHammer} style={{display: 'inline'}}/>ools</Text>
                     </a>
                 </Link>
                 <IconButton icon={colorMode == 'light'? <BsSunFill/> : <BsMoonFill/>} aria-label={"dark / light"} onClick={toggleColorMode}></IconButton>
